8bca64a1c345eb65b562d5b229f21c69ac298467
Vinay <vinay.mishra@gmail.com>
2016-02-04 00:42:38 -0800

    testing..

---
+++ src/main/java/beegrp/App.java
@@ -0,0 +1,13 @@
+package beegrp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/scala/Simple.scala
@@ -0,0 +1,3 @@
+object Simple extends App{
+  println("kdkdd")
+}
